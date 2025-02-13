ff0f34cac0ada763c574cd81e55babe4c5d70d14
Marcelo <cordolla10@gmail.com>
2025-02-13 14:53:17 -0300

    git project default

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
 import fastify from "fastify";
+import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
-app.get("/hello", () =>{
-  return "olá oi";
+app.get("/hello", async () =>{
+  const transactions = await knex("transactions").insert({
+    id: crypto.randomUUID(),
+    title: "Test Transaction",
+    amount: 100.00,
+  }).returning("*");
+
+  return transactions;
 });
 
-app.listen({
-  port: 3336,
-}).then(() => {
-  console.log("http server running");
-});
+app
+  .listen({
+    port: env.PORT,
+  }).then(() => {
+    console.log("http server running");
+  });
